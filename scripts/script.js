28df73e80e7dd7534a70bc48a0c6320a5b559d30
cjwit <chris.witulski@gmail.com>
2020-12-14 14:01:46 -0500

    Add communal example

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,7 @@ import { examples } from './examples.js';
 const example1 = new ExampleWithSampler("example1", examples[1]);
 const example2 = new ExampleWithSlider("example2", "Drag to change the frequency");
 const example3 = new ExampleWithScales("example3", examples[3]);
-const example4 = new ExampleHeaderOnly("example4", "Odd melodies");
 
 // part 2
-const example5 = new ExampleHeaderOnly("example5", "Melodies, worn, surprise, jarring");
-const example6 = new ExampleHeaderOnly("example6", "Same melodies, with countermelodies");
+const example4 = new ExampleHeaderOnly("example4", "Melodies, worn, surprise, jarring");
+const example5 = new ExampleHeaderOnly("example5", "Same melodies, with countermelodies");
